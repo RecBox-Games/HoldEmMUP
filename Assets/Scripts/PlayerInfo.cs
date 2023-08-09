be4af41dab960388c54b355311e837e79278595f
DigitalSRC <60196645+DigitalSRC@users.noreply.github.com>
2023-08-09 16:46:01 -0700

    Implements Playmats
    Playmats are the color spaces to represent each player.
    
    Added some script to playerInfo and made it hold a list of all the materials(colors) that the mat can change too. playerColor takes an integer so make sure that the server sends one over.

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerInfo : MonoBehaviour
 {
 
-    [SerializeField]
+    [SerializeField] Object playmat;
+    [SerializeField] List<Material> materials = new List<Material>();
+    [SerializeField] int playerColor;
+
     public Text PlayerName;
     public string playerName;
+    
 
         // Start is called before the first frame update
     void Start()
     {
         playerName = "Waiting to Join";
+
+        // This needs to be changed to how ever the server sends a players color over.
+        playerColor = 1;
+
+
     }
     // Update is called once per frame
     void Update()
     {
-      PlayerName.text = playerName;
+        PlayerName.text = playerName;
+        playmat.GetComponent<MeshRenderer>().material = materials[playerColor];
+
     }
+
 
 }
---
+++ Assets/Scripts/PlaymatController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaymatController : MonoBehaviour
+{
+    [SerializeField] PlayerInfo playerInfo;
+    [SerializeField] Material material;    
+    // public Material material;
+    public GameObject Object; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.GetComponent<MeshRenderer>().material = material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
